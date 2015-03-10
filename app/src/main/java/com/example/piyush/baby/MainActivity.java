414aaf6ec422dde5197be59b42aec96087acb924
Piyush <pgkaila@gmail.com>
2015-03-10 23:54:08 +0530

    Understanding Activity lifecycle

--- app/src/main/java/com/example/piyush/baby/MainActivity.java
+++ app/src/main/java/com/example/piyush/baby/MainActivity.java
@@ -1,39 +1,55 @@
 package com.example.piyush.baby;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        System.out.println("Activity created...");
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onStart() {
+        super.onStart();
+        System.out.println("Activity started...");
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("Activity restarted...");
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("Activity resumed...");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("Activity paused...");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("Activity stopped...");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("Activity destroyed...");
     }
 }
